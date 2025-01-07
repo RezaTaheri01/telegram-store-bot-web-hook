a5dddee417e16036989df9b534eb13ec3e32173d
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-08 00:22:35 +0330

    Update urls.py
    straightforward

--- telegram_store/telegram_store/urls.py
+++ telegram_store/telegram_store/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from bot_module.views import webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('payment/', include('payment.urls')),
-    path('telegram_bot/', include('bot_module.urls')),
+    path('webhook/', webhook, name='webhook'),
     path('', views.HomePage.as_view(), name="home_page"),
 ]
