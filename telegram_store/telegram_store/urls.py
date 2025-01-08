5b43a82eb0b0f86f6dcd3a1f71c7b729766a8888
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-09 00:17:21 +0330

    Update urls.py

--- telegram_store/telegram_store/urls.py
+++ telegram_store/telegram_store/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-# from bot_module.views import webhook # Simple
-from bot_module.bot import webhook  # Game Store
+from bot_module.views import webhook  # Simple
 
 urlpatterns = [
     path('admin/', admin.site.urls),
