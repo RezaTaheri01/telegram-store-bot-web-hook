fb8f682c0f59c6e0ff17dded541f205b75c956d9
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-08 20:10:09 +0330

    Update apps.py

--- telegram_store/bot_module/apps.py
+++ telegram_store/bot_module/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class BotModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot_module'
+
+    def ready(self):
+        # Initialize the bot application once
+        # from bot_module.bot import app  # Import the global app instance
+        from bot_module.views import app  # Import the global app instance
+        import asyncio
+
+        async def start_bot():
+            await app.initialize()
+            await app.start()
+
+        asyncio.run(start_bot())
