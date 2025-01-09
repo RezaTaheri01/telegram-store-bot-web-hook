7ec0c0e99f03e7a4c866e1faf482f5277d15dc16
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-10 00:25:51 +0330

    Update urls.py
    main django not get webhook url anymore

--- telegram_store/telegram_store/urls.py
+++ telegram_store/telegram_store/urls.py
@@ -11,17 +11,15 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from bot_module.views import webhook  # Simple
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('payment/', include('payment.urls')),
-    path('webhook/', webhook, name='webhook'),
     path('', views.HomePage.as_view(), name="home_page"),
 ]
