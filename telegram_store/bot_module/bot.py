7a49abe0fabc836db96dc379701c461f4d832d2c
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-11 00:51:03 +0330

    Update bot.py

--- telegram_store/bot_module/bot.py
+++ telegram_store/bot_module/bot.py
@@ -53,6 +53,10 @@ from telegram.ext import (
     ExtBot
 )
 
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+import json
+
 # endregion
 
 
@@ -775,6 +779,49 @@ async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
     await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML)
 
 
+@csrf_exempt
+async def telegram(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        return HttpResponse(status=404)
+
+    logger.info("Telegram")
+    try:
+        """Handle incoming Telegram updates by putting them into the `update_queue`"""
+        await ptb_application.update_queue.put(
+            Update.de_json(data=json.loads(request.body), bot=ptb_application.bot)
+        )
+    except Exception as e:
+        logger.error(f"Error in ptb_application.update_queue.put: {e}")
+
+    return HttpResponse(status=200)  # Return a successful response
+
+
+@csrf_exempt
+async def custom_updates(request: HttpRequest) -> HttpResponse:
+    """
+    Handle incoming webhook updates by also putting them into the `update_queue` if
+    the required parameters were passed correctly.
+    """
+    try:
+        user_id = int(request.GET["user_id"])
+        payload = request.GET["payload"]
+    except KeyError:
+        return HttpResponseBadRequest(
+            "Please pass both `user_id` and `payload` as query parameters.",
+        )
+    except ValueError:
+        return HttpResponseBadRequest("The `user_id` must be a string!")
+
+    await ptb_application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
+    return HttpResponse()
+
+
+@csrf_exempt
+async def health(_: HttpRequest) -> HttpResponse:
+    """For the health endpoint, reply with a simple plain text message."""
+    return HttpResponse("The bot is still running fine :)")
+
+
 # endregion
 
 # Set up PTB application and a web application for handling the incoming requests.
@@ -835,14 +882,13 @@ async def main() -> None:
     )
 
     # Pass webhook settings to telegram
-    await ptb_application.bot.set_webhook(url=f"{URL}/webhook/telegram", allowed_updates=Update.ALL_TYPES)
+    # await ptb_application.bot.set_webhook(url=f"{URL}/webhook/telegram", allowed_updates=Update.ALL_TYPES)
 
     # Run application and webserver together
     async with ptb_application:
         await ptb_application.start()
         await webserver.serve()
         await ptb_application.stop()
-
 
 # if __name__ == "__main__":
 #     asyncio.run(main())
