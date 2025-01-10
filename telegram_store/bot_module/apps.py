968d7b6f4158c03163297894ea9ae0b2ad230dfe
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-11 01:52:23 +0330

    Update apps.py

--- telegram_store/bot_module/apps.py
+++ telegram_store/bot_module/apps.py
@@ -8,11 +8,6 @@ class BotModuleConfig(AppConfig):
 
     def ready(self):
         from .bot import main
-
-        # We will schedule the async main function outside the django setup
-        loop = asyncio.get_event_loop()
-        if loop.is_running():
-            # If there is already a running loop (like when running in an async context), don't create a new one
-            loop.create_task(main())  # Create task instead of running it immediately
-        else:
-            asyncio.run(main())  # Only run main() if there is no running loop
+        import threading
+        thread = threading.Thread(target=asyncio.run, args=(main(),))
+        thread.start()
