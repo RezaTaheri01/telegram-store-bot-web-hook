7f3b52c73c69b4ae0b059f297f522b2aedf69cbc
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-11 00:33:22 +0330

    Update views.py

--- telegram_store/bot_module/views.py
+++ telegram_store/bot_module/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, Http404
+from telegram import Update
 import json
-import logging
-from bot_module.bot import ptb_application, Update
-
-# Set up logger
-logger = logging.getLogger(__name__)
+from bot_module.bot import ptb_application, WebhookUpdate, logger
 
 
 @csrf_exempt
@@ -23,3 +20,29 @@ async def telegram(request: HttpRequest) -> HttpResponse:
         logger.error(f"Error in ptb_application.update_queue.put: {e}")
 
     return HttpResponse(status=200)  # Return a successful response
+
+
+@csrf_exempt
+async def custom_updates(request: HttpRequest) -> HttpResponse:
+    """
+    Handle incoming webhook updates by also putting them into the `update_queue` if
+    the required parameters were passed correctly.
+    """
+    try:
+        user_id = int(request.GET["user_id"])
+        payload = request.GET["payload"]
+    except KeyError:
+        return HttpResponseBadRequest(
+            "Please pass both `user_id` and `payload` as query parameters.",
+        )
+    except ValueError:
+        return HttpResponseBadRequest("The `user_id` must be a string!")
+
+    await ptb_application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
+    return HttpResponse()
+
+
+@csrf_exempt
+async def health(_: HttpRequest) -> HttpResponse:
+    """For the health endpoint, reply with a simple plain text message."""
+    return HttpResponse("The bot is still running fine :)")
