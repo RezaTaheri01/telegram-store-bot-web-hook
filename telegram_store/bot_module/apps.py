17fd264b6e67ad1f8e60eea9fdfcc32005069c0f
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-12 12:11:57 +0330

    Update apps.py

--- telegram_store/bot_module/apps.py
+++ telegram_store/bot_module/apps.py
@@ -7,8 +7,11 @@ class BotModuleConfig(AppConfig):
     name = 'bot_module'
 
     # Todo: Search about ready function and the method that used
+    # The current ready(self) approach is functional but not ideal for production.
+    # best way is to run it separately. (main branch)
     def ready(self):
         from .bot import main
         import threading
         thread = threading.Thread(target=asyncio.run, args=(main(),))
+        thread.daemon = True  # Ensure the thread doesn't block app shutdown
         thread.start()
