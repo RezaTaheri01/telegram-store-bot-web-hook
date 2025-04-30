c41074170da2dd39fc2612e5a32c134ffc592f2e
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-04-30 23:50:03 +0330

    Update build.sh

--- telegram_store/build.sh
+++ telegram_store/build.sh
@@ -13,23 +13,23 @@ DJANGO_SUPERUSER_USERNAME=admin
 DJANGO_SUPERUSER_EMAIL=admin@example.com
 DJANGO_SUPERUSER_PASSWORD=admin12345678
 
-# Use environment variables to create the superuser non-interactively
-python manage.py createsuperuser \
-    --noinput \
-    --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL
-
-# Use a Python script to set the password
-echo "Setting password for superuser..."
-python << END
+# Create superuser if it doesn't exist
+python manage.py shell << END
 from django.contrib.auth import get_user_model
+from django.conf import settings
+import django
+
+django.setup()
 User = get_user_model()
-user, created = User.objects.get_or_create(username="$DJANGO_SUPERUSER_USERNAME", email="$DJANGO_SUPERUSER_EMAIL")
-if created:
-    user.set_password("$DJANGO_SUPERUSER_PASSWORD")
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
+username = "$DJANGO_SUPERUSER_USERNAME"
+email = "$DJANGO_SUPERUSER_EMAIL"
+password = "$DJANGO_SUPERUSER_PASSWORD"
+
+if not User.objects.filter(username=username).exists():
+    user = User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
 END
 
 echo "Setup complete!"
